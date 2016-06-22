3dd45e6b61f022dc2ffec98ef9ef5d4c5fd5291d
ChrisIngoeTlc <christopher.ingoe@thomsonlocal.com>
2016-06-22 22:19:18 +0100

    Do not close the side nav when a menu item is clicked

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -107,6 +107,6 @@ angular.element(document).ready(function () {
     $('.button-collapse').sideNav({
         menuWidth: 240, // Default is 240
         edge: 'left', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
 });
